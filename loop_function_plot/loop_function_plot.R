a713faa5f9a9e33c00390225772d52c853e8eabf
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2021-02-24 00:14:56 -0300

    add script to plot from list

---
+++ loop_function_plot/loop_function_plot.R
@@ -0,0 +1,40 @@
+library(qualR)
+
+
+# Downloading O3 data for one week ----------------------------------------
+
+start_date <- "06/01/2020"
+end_date <- "12/01/2020"
+
+rj_aqs <- monitor_ar_aqs
+
+O3 <- lapply(rj_aqs$code, MonitorArRetrieve,
+             start_date = start_date,
+             end_date = end_date,
+             param = "O3")
+
+
+
+# Function plot data in list ----------------------------------------------
+
+
+PlotFromList <- function(df, aqs_data=rj_aqs){
+  code <- unique(df$aqs)
+  title <- aqs_data$name[aqs_data$code == code]
+  file_name <- paste0(code, "_o3.png")
+  
+  if (is.na(unique(df$O3)[1])){
+    print("No data available")
+  } else {
+    png(file_name, height = 550, width = 760)
+    plot(df$date, df$O3, t = "l", col = "Red", lwd = 2.5,
+         main = title,
+         xlab = " ",
+         ylab = expression("O"[3] * " (" * mu * "g m"^-3 * ")"))
+    dev.off()
+  }
+}
+
+
+# Plotting  ---------------------------------------------------------------
+lapply(O3, PlotFromList)
