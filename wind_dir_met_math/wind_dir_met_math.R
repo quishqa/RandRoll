f8aa4b01fb09d8fc8637cd09aaf62e9fce1c123c
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-05-03 18:15:50 -0300

    Script wind direction and components

--- wind_dir_met_math/wind_dir_met_math.R
+++ wind_dir_met_math/wind_dir_met_math.R
@@ -1,15 +1,44 @@
-deg2rad <- function(angle){
-  rad <- angle * pi / 180
+deg2rad <- function(deg){
+  rad <- deg * pi / 180
   return(rad)
 }
 
-met2math <- function(winds){
-  winds$u <- - winds$ws * sin(deg2rad(winds$wd))
-  winds$v <- - winds$ws * cos(deg2rad(winds$wd))
-  return(winds)
+rad2deg <- function(rad){
+  deg <- rad * 180 / pi
+  return(deg)
+}
+
+wind_components <- function(ws, wd){
+  u <- -ws * sin(deg2rad(wd))
+  v <- -ws * cos(deg2rad(wd))
+  ans <- data.frame(u = u, v = v)
+  return(ans)
+}
+
+wd_from_components <- function(u, v){
+  wd <- rad2deg(atan2(-u, -v))
+  wd[wd < 0] <- wd[wd < 0] + 360
+  return(wd)
 }
 
 test_data <- data.frame(
   ws = rep(1, 4),
   wd = c(0, 90, 180, 270)
 ) 
+
+test_data$u <- wind_components(test_data$ws, test_data$wd)$u
+test_data$v <- wind_components(test_data$ws, test_data$wd)$v
+
+test_uv <- data.frame(
+  u = c(0, 0, 1, 1, -1, -1, 1, -1),
+  v = c(1, -1, 1, -1, 1, -1, 0, 0),
+  wd_ori = c(180, 0, 225, 315, 135, 45, 270, 90)
+)
+
+test_uv$wd <- wd_from_components(test_uv$u, test_uv$v)
+
+
+
+
+
+
