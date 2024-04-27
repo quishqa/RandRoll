399f1426eed70cbb849d367ab529a32cda41e2ef
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2024-04-27 13:51:06 -0300

    feat: subset replace post

--- subset_replace/subset_replace.R
+++ subset_replace/subset_replace.R
@@ -16,5 +16,28 @@ daily_pol <- data.frame(
   co = co
 )
 
-day_over_who <- subset(dayly_pm25, pm25 > 15 & co > 4)
-na_over_who <- subset(dayly_pm25, pm25 < 0)
+# Finding over the air quality standard for PM25
+over_who_pm25 <- subset(daily_pol, subset = pm25 > 15)
+nrow(over_who_pm25)
+
+# Finding over the air quality standard for PM25 and select
+# columns
+over_who_pm25 <- subset(daily_pol,
+                        subset = pm25 > 15,
+                        select = c("date", "pm25"))
+
+# Finding days over PM25 and CO air quality standard
+day_over_who <- subset(daily_pol,
+                       subset = pm25 > 15 & co > 4)
+nrow(day_over_who)
+
+# Cleaning negative data
+daily_pol_clean <- replace(daily_pol,
+                           daily_pol < 0,
+                           NA)
+
+daily_pol$pm25 <- replace(daily_pol$pm25,
+                          daily_pol$pm25,
+                          NA)
+
+
